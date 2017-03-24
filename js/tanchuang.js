24c380cdfdf9dc16f16c4c1360db0ac911490fa3
tofay <1430157192@qq.com>
2017-03-24 17:01:32 +0800

    add

--- js/tanchuang.js
+++ js/tanchuang.js
@@ -1,7 +1,20 @@
 $(function(){
 	apply.init();
-	
-	
+   $(".button .leftn").click(function(){
+	   $(".apply_window").show();
+   })
+	$(".apply_window .close").click(function(){
+		$(".apply_window").hide();
+	});
+
+	var num=0;
+	setInterval(function(){
+		if(num==3){
+			num=-1;
+		}
+		num++;
+		$(".posit img").prop("src","../imgs/a"+num+".png")
+	},500);
 })
 var apply={
 		init:function(){
